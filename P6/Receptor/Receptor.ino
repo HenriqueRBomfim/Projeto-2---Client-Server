67deaa207fd0621f7d9801dc8a88f1186a731711
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2023-10-20 17:10:10 -0300

    P6 completo e início do P7

--- P6/Receptor/Receptor.ino
+++ P6/Receptor/Receptor.ino
@@ -1,44 +1,40 @@
-const int rxPin = 7; // Pino digital usado para receber os dados
-const int baudrate = 9600; // Taxa de baud (bps)
+int pinoServidor = 7; // número do pino usado pelo Arduino para receber dados
+float baudrate = 9600; // 9600 bits por segundo
+int mensagem;
 
 void setup() {
-  pinMode(rxPin, INPUT);
-  Serial.begin(baudrate);
+  pinMode(pinoServidor, INPUT); // define o pino digital como entrada, já que o servidor está recebendo dados do cliente
+  Serial.begin(baudrate); // inicia a porta serial, configurando a taxa de recebimento de dados para 9600 bits por segundo
+}
+
+float aguardarTempo(float tempoDeEspera = 1, float baudrate = 9600, float T0 = 0){
+  double clock = 1 / (16 * pow(10, 6)); // tempo de 1 clock (T = 1/frequência), onde a frequência = 21MHz
+  double T = 1 / baudrate; // tempo entre cada clock, em segundos
+  int numeroDeClocks = floor(T / clock) + 1; // número de clocks a esperar, arredondado para o inteiro mais próximo
+  for (int i = 0; i < int(numeroDeClocks * tempoDeEspera); i++){ asm("NOP"); } // aguarde o tempo especificado
 }
 
 void loop() {
-  receberByteSerial();
-}
-
-void receberByteSerial() {
-  while (digitalRead(rxPin) == LOW); // Aguarda o bit de início
-
-  delayMicroseconds(1000000 / baudrate / 2); // Move-se para o meio do bit
-  byte dado = 0;
-
-  // Lê os 8 bits de dados (LSB primeiro)
-  for (int i = 0; i < 8; i++) {
-    delayMicroseconds(1000000 / baudrate);
-    dado |= (digitalRead(rxPin) << i);
-  }
-
-  delayMicroseconds(1000000 / baudrate / 2); // Move-se para o meio do bit de paridade
-  int paridadeRecebida = digitalRead(rxPin);
-
-  delayMicroseconds(1000000 / baudrate / 2); // Move-se para o meio do bit de parada
-  int bitParada = digitalRead(rxPin);
-
-  // Verifica a paridade (paridade par)
-  int paridadeCalculada = 0;
-  for (int i = 0; i < 8; i++) {
-    if ((dado >> i) & 1) {
-      paridadeCalculada ^= 1; // XOR para calcular a paridade
+  if (digitalRead(pinoServidor) == 0){
+    int quantidadeDe1s = 0;
+    aguardarTempo(1.5); // posicionando o momento de leitura dos bits no meio de cada bit (portanto, o bit de início é ignorado)
+    for (int i = 0; i < 8; i++){
+      int bitAtual = digitalRead(pinoServidor); // leia o bit atual
+      aguardarTempo(); // sempre chamado após a leitura de um bit
+      if (bitAtual == 1){ quantidadeDe1s++; } // conte a quantidade de 1s na mensagem
+      mensagem |= (bitAtual << i); // adicione o bit atual à mensagem
     }
-  }
-
-  // Verifica se a paridade recebida coincide com a paridade calculada
-  if (paridadeRecebida == paridadeCalculada && bitParada == HIGH) {
-    Serial.print("Byte Recebido: ");
-    Serial.println(char(dado));
+    int bitDeParidadeRecebido = digitalRead(pinoServidor); // leia o bit de paridade recebido
+    int bitDeParidadeCalculado = (quantidadeDe1s % 2); // calcule o bit de paridade a partir da mensagem enviada
+    if (bitDeParidadeRecebido == bitDeParidadeCalculado){ // se o bit de paridade recebido for igual ao bit de paridade calculado, a mensagem está correta
+      Serial.print("Dados recebidos: ");
+      Serial.println(char(mensagem));
+      Serial.println("O bit de paridade está correto");
+    } else { // se o bit de paridade recebido for diferente do bit de paridade calculado, a mensagem está incorreta
+      Serial.println("ERRO. O bit de paridade NÃO está correto");
+    }
+    for (int i = 0; i < 2084; i++){ // envie
+      aguardarTempo();
+    }
   }
 }
--- P6/Transmissor/Transmissor.ino
+++ P6/Transmissor/Transmissor.ino
@@ -1,14 +1,46 @@
-const int txPin = 7; // Pino digital usado para transmitir os dados
-const int baudrate = 9600; // Taxa de baud (bps)
-const byte dado = 'A'; // Dado a ser transmitido (no exemplo, 'A')
+int pinoCliente = 7; // número do pino usado pelo Arduino para enviar dados
+char mensagem = 'G'; // letra 'A' em hexadecimal
+int mensagemBinaria = int(mensagem); // letra 'A' convertida de hexadecimal para binário
+float baudrate = 9600; // 9600 bits por segundo
 
 void setup() {
-  pinMode(txPin, OUTPUT);
-  Serial.begin(baudrate);
+  pinMode(pinoCliente, OUTPUT); // define o pino digital como saída, já que o cliente está enviando para o servidor
+  Serial.begin(baudrate); // inicia a porta serial, configurando a taxa de envio de dados para 9600 bits por segundo
+  digitalWrite(pinoCliente, HIGH); // comece com o pino em nível alto (1)
+}
+
+float aguardarTempo(float tempoDeEspera = 1, float baudrate = 9600, float T0 = 0){
+  double clock = 1 / (16 * pow(10, 6)); // tempo de 1 clock (T = 1/frequência), onde a frequência = 16MHz
+  double T = 1 / baudrate; // tempo entre cada clock, em segundos
+  int numeroDeClocks = floor(T / clock) + 1; // número de clocks a esperar, arredondado para o inteiro mais próximo
+  for (int i = 0; i < int(numeroDeClocks * tempoDeEspera); i++){ asm("NOP"); } // aguarde o tempo especificado
 }
 
 void loop() {
-  digitalWrite(txPin, HIGH); // ativa o pino digital 13         // espera por um segundo
-  delay(1000)
-  digitalWrite(txPin, LOW);  // desativa o pino digital 13 
+  int quantidadeDe1s = 0; // contador para a quantidade de 1s na mensagem
+
+  digitalWrite(pinoCliente, 0); // envie o bit de início
+  aguardarTempo(); // sempre chamado após o envio de um bit
+
+  for (int i = 0; i < 8; i++){ // envie os 8 bits (1 byte) da mensagem
+    int bitAtual = 1 & (mensagemBinaria >> i); // obtenha o bit atual da mensagem, começando pelo bit menos significativo
+    digitalWrite(pinoCliente, bitAtual); // envie o bit atual
+    if (bitAtual == 1){ quantidadeDe1s++; } // conte a quantidade de 1s na mensagem
+    aguardarTempo(); // sempre chamado após o envio de um bit
+  }
+
+  int bitDeParidade = quantidadeDe1s % 2; // se o resto for igual a 0, então é par, senão é ímpar
+  digitalWrite(pinoCliente, bitDeParidade); // envie o bit de paridade
+  aguardarTempo(); // sempre chamado após o envio de um bit
+
+  digitalWrite(pinoCliente, 1); // envie o bit de parada
+  aguardarTempo(); // sempre chamado após o envio de um bit
+
+  Serial.print("Dados enviados: ");
+  Serial.println(mensagem); // imprime a mensagem enviada (em hexadecimal)
+
+  for (int i = 0; i < 2084; i++){ // envie
+    aguardarTempo();
+  }
+  
 }
